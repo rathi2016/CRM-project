6c24cb492ccf9671d5c315979278dd6f330ca0da
Rathi <github email address>
2017-11-09 16:08:02 -0500

    added firebase

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import firebase from 'firebase';
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!!!!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
